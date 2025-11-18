5bfbead14ee696f674b44ebafa5d20b03636cd1f
Vithusa Muttaiya <6232542@johnabbottcollege.net>
2025-11-18 12:23:31 -0500

    added migrations

--- CondoLounge/obj/Debug/net8.0/CondoLounge.AssemblyInfo.cs
+++ CondoLounge/obj/Debug/net8.0/CondoLounge.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CondoLounge")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8b0024a277d12105cd460f9e4ad762e1961a9b3e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5633bfe4d65b69f10650ac3101031b882c4b8bb0")]
 [assembly: System.Reflection.AssemblyProductAttribute("CondoLounge")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CondoLounge")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
